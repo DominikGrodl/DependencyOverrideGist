0d58d3c4483923d3eb1316ecf40806a97537a430
Dominik Grodl <dominikgrodl@icloud.com>
2023-11-02 15:41:10 +0100

    Initial Commit

---
+++ DependencyOverrideTest/DependencyOverrideTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyOverrideTestApp.swift
+//  DependencyOverrideTest
+//
+//  Created by Dominik Grodl on 02.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DependencyOverrideTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
